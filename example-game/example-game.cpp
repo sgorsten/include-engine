d32714af21bfb6ab490c743e01f9b67d14577447
Sterling Orsten <sgorsten@gmail.com>
2017-03-27 23:26:10 -0700

    Use a shader for rendering.

--- example-game/example-game.cpp
+++ example-game/example-game.cpp
@@ -13,6 +13,50 @@ using namespace linalg::aliases;
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
+GLuint compile_shader(GLenum type, const char * source)
+{
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+
+    GLint status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if(status != GL_TRUE)
+    {
+        GLint length;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+
+        std::string log(length, ' ');
+        glGetShaderInfoLog(shader, length, nullptr, &log[0]);
+        glDeleteShader(shader);
+        throw std::runtime_error("compile error " + log);
+    }
+
+    return shader;
+}
+
+GLuint link_program(std::initializer_list<GLuint> shaders)
+{
+    GLuint program = glCreateProgram();
+    for(auto shader : shaders) glAttachShader(program, shader);
+    glLinkProgram(program);
+
+    GLint status;
+    glGetProgramiv(program, GL_LINK_STATUS, &status);
+    if(status != GL_TRUE)
+    {
+        GLint length;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &length);
+
+        std::string log(length, ' ');
+        glGetProgramInfoLog(program, length, nullptr, &log[0]);
+        glDeleteProgram(program);
+        throw std::runtime_error("link error " + log);
+    }
+
+    return program;
+}
+
 int main() try
 {
     std::ifstream in("assets/helmet-mesh.fbx", std::ifstream::binary);
@@ -26,6 +70,40 @@ int main() try
     auto win = glfwCreateWindow(640, 480, "Example Game", nullptr, nullptr);
     glfwMakeContextCurrent(win);
     glewInit();
+
+    auto program = link_program({
+        compile_shader(GL_VERTEX_SHADER, R"(#version 450
+            uniform mat4 u_model_matrix;
+            uniform mat4 u_view_proj_matrix;
+            layout(location = 0) in vec3 v_position;
+            layout(location = 1) in vec3 v_normal;
+            layout(location = 2) in vec2 v_texcoord;
+            out vec3 position;
+            out vec3 normal;
+            out vec2 texcoord;
+            void main() 
+            { 
+                position = (u_model_matrix * vec4(v_position, 1)).xyz;
+                normal = normalize((u_model_matrix * vec4(v_normal, 0)).xyz);
+                texcoord = v_texcoord;
+                gl_Position = u_view_proj_matrix * vec4(position, 1); 
+            }
+        )"),
+        compile_shader(GL_FRAGMENT_SHADER, R"(#version 450
+            uniform sampler2D u_albedo;
+            in vec3 position;
+            in vec3 normal;
+            in vec2 texcoord;
+            layout(location = 0) out vec4 f_color;
+            void main()
+            {
+                vec3 normal_vec = normalize(normal);
+                vec3 light_vec = normalize(vec3(1,5,2));
+                float diffuse_factor = max(dot(normal_vec, light_vec), 0);                
+                f_color = texture(u_albedo, texcoord) * diffuse_factor;
+            }
+        )")
+    });
 
     int x, y;
     auto image = stbi_load("assets/helmet-albedo.jpg", &x, &y, nullptr, 3);
@@ -75,43 +153,28 @@ int main() try
         glfwGetFramebufferSize(win, &width, &height);
         const auto proj_matrix = linalg::perspective_matrix(1.0f, (float)width/height, 1.0f, 1000.0f);
         const auto view_matrix = inverse(pose_matrix(camera_orientation, camera_position));
+        const auto view_proj_matrix = mul(proj_matrix, view_matrix);
 
         // Clear the viewport
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glMatrixMode(GL_PROJECTION);
-        glLoadMatrixf(&proj_matrix.x.x);
-
-        glMatrixMode(GL_MODELVIEW);
-        glLoadMatrixf(&view_matrix.x.x);
-
-        auto light_dir = normalize(float4{1,5,2,0});
-        glEnable(GL_LIGHT0);
-        glLightfv(GL_LIGHT0, GL_POSITION, &light_dir.x);
-        glEnable(GL_LIGHTING);
+                
         glEnable(GL_DEPTH_TEST);
+        glUseProgram(program);
+        glUniformMatrix4fv(glGetUniformLocation(program, "u_view_proj_matrix"), 1, GL_FALSE, &view_proj_matrix.x.x);
 
         for(auto & m : models)
         {
             const auto model_matrix = m.get_model_matrix();
-            const auto model_view_matrix = mul(view_matrix, model_matrix);
-            glLoadMatrixf(&model_view_matrix.x.x);
+            glUniformMatrix4fv(glGetUniformLocation(program, "u_model_matrix"), 1, GL_FALSE, &model_matrix.x.x);
 
             for(auto & g : m.geoms)
             {
-                glEnable(GL_TEXTURE_2D);
-                glBegin(GL_TRIANGLES);
-                for(auto & triangle : g.triangles)
-                {
-                    for(auto index : triangle) 
-                    {
-                        glTexCoord2fv(&g.vertices[index].texcoord.x);
-                        glNormal3fv(&g.vertices[index].normal.x);
-                        glVertex3fv(&g.vertices[index].position.x);
-                    }
-                }
-                glEnd();
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(fbx::geometry::vertex), &g.vertices.data()->position);
+                glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(fbx::geometry::vertex), &g.vertices.data()->normal);
+                glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(fbx::geometry::vertex), &g.vertices.data()->texcoord);
+                for(GLuint i : {0,1,2}) glEnableVertexAttribArray(i);
+                glDrawElements(GL_TRIANGLES, g.triangles.size()*3, GL_UNSIGNED_INT, g.triangles.data());
             }
         }
 
